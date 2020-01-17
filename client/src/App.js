47ce1a0ef3b03367d3ec895637165012999d596f
lydiecherilus <lydiecherilus@yahoo.fr>
2020-01-17 13:41:45 -0500

    List Player Displayed - Hook Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import Axios from 'axios'
 import './App.css';
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PlayersList from './components/PlayersList'
+import Navigation from './components/Navigation'
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    data: [],
+    name: "",
+    country: "",
+  };
 }
+  componentDidMount() {
+    Axios.get("http://localhost:5000/api/players")
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          data: response.data,
+          name: response.data.name,
+          country: response.data.country
+        });
+      })
+      .catch(error => console.log('error'));
+  }
 
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+
+        <PlayersList 
+                data={this.state.data}
+                name={this.state.name}
+                country={this.state.country}
+            />
+      </div>
+    );
+  }
+}
 export default App;
---
+++ client/src/components/PlayersList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PlayerCard from  "./PlayerCard";
+
+class PlayersList extends React.Component {
+
+    render(props) {
+        return (
+            <div className="player_list">
+                {this.props.data.map(player => {
+                    return (
+                        <PlayerCard
+                            key={player.name}
+                            name={player.name}
+                            country={player.country}
+                        />
+                    );
+                })}
+            </div>
+        );
+    }
+}
+export default PlayersList;
